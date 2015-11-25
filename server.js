ba8b791e6ee4b53b812ed23d590c8cbdff322050
Blake Hagen <blakehagen@gmail.com>
2015-11-25 12:48:17 -0700

    initial file setup

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
-// test
+// EXPRESS AND MONGOOSE CONFIG FILES //
+var express = require('./config/express');
+var mongoose = require('./config/mongoose');
 
+// RUN EXPRESS AND MONGOOSE //
+var app = express();
+var db = mongoose();
+
+
+
+
+
+
+
+
+
+
+
+// PORT //
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+    console.log('Listenting on port ' + port);
+});
